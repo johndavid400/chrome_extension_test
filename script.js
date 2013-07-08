57c1be27147b37d23426b6e932ce4007220e7c0c
JD <johndavid400@gmail.com>
2013-07-07 22:09:57 -0500

    added route for getting user id

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ console.log("Check me out... no seriously, check me out.");
 
 $(document).ready(function(){
   var div = document.createElement("div");
-  div.innerHTML += "FOO BAR";
+  var foo = $.get('https://staging.yesboxx.com/retrieve_user_id');
+  console.log(foo);
+  div.innerHTML += foo;
   document.body.appendChild(div);
 });
 
