c83dd2f54356479b47c5cc3e22ee84aa7507866d
JD <johndavid400@gmail.com>
2013-07-07 21:18:53 -0500

    updating stuff

--- fooBar.js
+++
@@ -1,1 +0,0 @@
-console.log("One step at a time");
---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("Check me out... no seriously, check me out.");
+
+$(document).ready(function(){
+  var div = document.createElement("div");
+  div.innerHTML += "FOO BAR";
+  document.body.appendChild(div);
+});
+
